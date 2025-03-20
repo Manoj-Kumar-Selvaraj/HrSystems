a7f56c238cc43e24a60157d8121fb17904a944a6
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-03-20 15:14:43 +0000

    ssl

--- Frontend/hrsystems/src/oktaConfig.js
+++ Frontend/hrsystems/src/oktaConfig.js
@@ -1,12 +1,12 @@
 import { OktaAuth } from "@okta/okta-auth-js";
 
-const oktaConfig = {
-  clientId: "your-client-id",
-  issuer: "https://dev-42945365.okta.com/oauth2/default",
-  redirectUri: window.location.origin + "/login/callback",
+const oktaAuth = new OktaAuth({
+  issuer: "https://dev-42945365.okta.com",
+  clientId: "0oanwc9h6wnB5YrOo5d7",
+  redirectUri: "https://okta.manoj-techworks.site/login/callback",
+  postLogoutRedirectUri: "https://okta.manoj-techworks.site",
   scopes: ["openid", "profile", "email"],
-};
-
-const oktaAuth = new OktaAuth(oktaConfig);
+  pkce: true, 
+});
 
 export default oktaAuth;
