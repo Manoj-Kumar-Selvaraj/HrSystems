0257d9ebd14a23c74a5926739f3137502ba11e0f
root <ss.mano1998@gmail.com>
2025-03-27 01:28:55 +0530

    added ashboard section

--- Backend/FASTApi/HrSystems/applications/main.py
+++ Backend/FASTApi/HrSystems/applications/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI, Depends
 from applications.auth.okta_auth import get_current_user
 from applications.routes import employee
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="HR System API")
+
+# Define allowed origins
+origins = [
+    "https://okta.manoj-techworks.site",  # Your frontend URL
+    "http://localhost:3000",  # If testing locally
+]
+
+# Add CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows requests from the specified origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
+
 
 # Protect all routes with authentication
 @app.get("/")
